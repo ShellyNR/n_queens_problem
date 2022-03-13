bdbee1b17bfc577c7c675fbec31a3648aaf95788
barshein <82324960+barshein@users.noreply.github.com>
2022-03-13 12:26:26 +0200

    Update genetic.py

--- genetic.py
+++ genetic.py
@@ -1,13 +1,23 @@
 import random
+import time
 
+def print_board(board):
+    for row in board:
+        print(row)
 
-def random_chromosome(size):  # making random chromosomes
+def create_final_board(nq, chrom_out):
+    board = []
+    for x in range(nq):
+        board.append([0] * nq)
+    for i in range(nq):
+        board[nq - chrom_out[i]][i] = 1
+    return board
+
+def init_population(nq):  # making random chromosomes
     return [random.randint(1, nq) for _ in range(nq)]
 
-
-def fitness(chromosome):
+def fitness(maxFitness,chromosome):
     horizontal_collisions = sum([chromosome.count(queen) - 1 for queen in chromosome]) / 2
-    diagonal_collisions = 0
 
     n = len(chromosome)
     left_diagonal = [0] * 2 * n
@@ -25,14 +35,12 @@ def fitness(chromosome):
             counter += right_diagonal[i] - 1
         diagonal_collisions += counter / (n - abs(i - n + 1))
 
-    return int(maxFitness - (horizontal_collisions + diagonal_collisions))  # 28-(2+3)=23
+    return int(maxFitness - (horizontal_collisions + diagonal_collisions))
 
+def probability(maxFitness,chromosome, fitness):
+    return fitness(maxFitness,chromosome) / maxFitness
 
-def probability(chromosome, fitness):
-    return fitness(chromosome) / maxFitness
-
-
-def random_pick(population, probabilities):
+def parents_pick(population, probabilities):
     populationWithProbabilty = zip(population, probabilities)
     total = sum(w for c, w in populationWithProbabilty)
     r = random.uniform(0, total)
@@ -41,14 +49,12 @@ def random_pick(population, probabilities):
         if upto + w >= r:
             return c
         upto += w
-    assert False, "Shouldn't get here"
-
+    exit() # in error case, should not happened
 
 def reproduce(x, y):  # doing cross_over between two chromosomes
     n = len(x)
     c = random.randint(0, n - 1)
     return x[0:c] + y[c:n]
-
 
 def mutate(x):  # randomly changing the value of a random index of a chromosome
     n = len(x)
@@ -57,63 +63,54 @@ def mutate(x):  # randomly changing the value of a random index of a chromosome
     x[c] = m
     return x
 
-
-def genetic_queen(population, fitness):
-    mutation_probability = 0.03
+def change_population(maxFitness,population, fitness,mut_prob):
     new_population = []
-    probabilities = [probability(n, fitness) for n in population]
+    probabilities = [probability(maxFitness,n, fitness) for n in population]
     for i in range(len(population)):
-        x = random_pick(population, probabilities)  # best chromosome 1
-        y = random_pick(population, probabilities)  # best chromosome 2
-        child = reproduce(x, y)  # creating two new chromosomes from the best 2 chromosomes
-        if random.random() < mutation_probability:
+        x = parents_pick(population, probabilities)
+        y = parents_pick(population, probabilities)
+        child = reproduce(x, y)
+        if random.random() < mut_prob:
             child = mutate(child)
-        # print_chromosome(child)
         new_population.append(child)
-        if fitness(child) == maxFitness: break
+        if fitness(maxFitness,child) == maxFitness: break
     return new_population
 
-
-# def print_chromosome(chrom):
-#     print("Chromosome = {},  Fitness = {}"
-#           .format(str(chrom), fitness(chrom)))
-#
-
-def geneticAlgo(N):
-    N = 8
-    global nq
-    global maxFitness
+def geneticAlgo(N,pop_size,mut_prob):
+    threshold_of_iter = N * 200
     nq = N
     maxFitness = (nq * (nq - 1)) / 2
-    population = [random_chromosome(nq) for _ in range(100)]
+    population = [init_population(nq) for _ in range(pop_size)]
     counter = 0
 
-    while not maxFitness in [fitness(chrom) for chrom in population]:
-        # print("=== Generation {} ===".format(generation))
-        population = genetic_queen(population, fitness)
-        # print("Maximum Fitness = {}".format(max([fitness(n) for n in population])))
+    while not maxFitness in [fitness(maxFitness,chrom) for chrom in population]:
+        population = change_population(maxFitness,population, fitness,mut_prob)
         counter += 1
-    chrom_out = []
-    print("iter counter {}!".format(counter))
+        if counter == threshold_of_iter:
+            print("again")
+            return geneticAlgo(N, pop_size,mut_prob)
+            # print("Failed")
+            # return 0
+
+
     for chrom in population:
-        if fitness(chrom) == maxFitness:
-            print("One of the solutions: ")
-            chrom_out = chrom
-            # print_chromosome(chrom)
+        if fitness(maxFitness,chrom) == maxFitness:
+            # chrom_out = chrom
+            # board = create_final_board(nq, chrom_out)
+            # print_board(board)
+            # print("iter counter {}".format(counter))
 
-    board = []
+            return counter
+            # print("Success")
+            # return 1
 
-    for x in range(nq):
-        board.append([0] * nq)
-
-    for i in range(nq):
-        board[nq - chrom_out[i]][i] = 1
-
-
-    def print_board(board):
-        for row in board:
-            print(row)
-
-
-    print()
-    print_board(board)
+def GA(N,pop_size,mut_prob):
+    if N == 2 or N == 3:
+        # print("Failed")
+        return 0, 0 # failed
+    start = time.time()
+    iter_counter = geneticAlgo(N,pop_size,mut_prob)
+    duration = time.time() - start
+    print("pop size: " +str(pop_size) + " mut prob: " +  str(mut_prob) +" N is: " + str(N) + " Runtime in second: " + str(duration)+ " iter: "+ str(iter_counter))
+    return duration,iter_counter
+    # return geneticAlgo(N,pop_size,mut_prob)
