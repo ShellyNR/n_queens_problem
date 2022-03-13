e6af7ad6387fee0ecc0cbf299905db084096dbda
barshein <82324960+barshein@users.noreply.github.com>
2022-03-13 23:51:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import backtracking
 import branch_and_bound
-import hillclimbing
+import hill_climbing
 import simulated_annealing
 import genetic
 
+def execute(algoNumber, N):
+    if algoNumber == 1:
+        backtracking.backtracking(N)
+    elif algoNumber == 2:
+        branch_and_bound.BNB(N)
+    elif algoNumber == 3:
+        hill_climbing.HC(N)
+    elif algoNumber == 4:
+        simulated_annealing.SA(N)
+    elif algoNumber == 5:
+        genetic.GA(N,100,0.3)
+    else:
+        print("wrong Number")
+
 if __name__ == '__main__':
     N = int(input("Enter size of N: "))
-    print("\nChoose from menu: \n1.backtracking\n2.branch and bound\n3.hillclimbing\n4.simulated annealing\n")
-    algoNumber = input("Enter algo number: ")
-#     N = 8
-#     algoNumber = 1
-#     sa.simulated_annealing(N)
-    algoDic = {
-        1 : backtracking.backtracking(N),
-        2 : branch_and_bound.BNB(N),
-#         3 : hillclimbing.hillclimbing(N),
-        4 : simulated_annealing.SA(N),
-        5 : genetic.geneticAlgo(N),
-
-    }[algoNumber]
+    print("\nChoose from menu: \n1.Backtracking Algorithm\n2.Branch And Bound\n3.Hill climbing\n4.Simulated Annealing\n5.Genetic Algorithm")
+    algoNumber = int(input("Enter algo number: "))
+    execute(algoNumber,N)
